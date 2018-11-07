2ae9a62a4617937a5c661abc2b1391992fc8717d
duanzhihua <duanzhihua@189.cn>
2018-11-07 21:13:18 +0800

    face

--- models.py
+++ models.py
@@ -7,9 +7,58 @@ import torch.nn.functional as F
 # can use the below import should you choose to initialize the weights of your Net
 import torch.nn.init as I
 
+# 3x3卷积核
+def conv3x3(in_planes,out_planes,stride=1):
+# =============================================================================
+#         in_channels (int): Number of channels in the input image
+#         out_channels (int): Number of channels produced by the convolution
+#         kernel_size (int or tuple): Size of the convolving kernel
+#         stride (int or tuple, optional): Stride of the convolution. Default: 1
+#         padding (int or tuple, optional): Zero-padding added to both sides of the input. Default: 0
+#         dilation (int or tuple, optional): Spacing between kernel elements. Default: 1
+#         groups (int, optional): Number of blocked connections from input channels to output channels. Default: 1
+#         bias (bool, optional): If ``True``, adds a learnable bias to the output. Default: ``True``
+# 
+# =============================================================================
+    return nn.Conv2d(in_planes,out_planes,kernel_size=3,stride=stride,padding=1,bias=False)
+
+# 基础网络模块
+class BasicBlock(nn.Module):
+    expansion = 1
+    
+    def __init__(self,in_planes,planes,stride=1,downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = conv3x3(in_planes, planes, stride)
+        self.bn1 = nn.BatchNorm2d(planes) # num_features: :math:`C` from an expected input of size :`(N, C, H, W)`   
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+        
+    def forward(self,x):
+        residual= x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)        
+        
+        if self.downsample is not None:
+            residual =self.downsample(x)
+            
+        out +=residual
+        out =self.relu(out)
+        return out 
+    
+class Flatten(nn.Module):      
+    def   forward(self,x):
+        return x.view(x.shape[0],-1)
+        
 
 class Net(nn.Module):
-
+    inplanes = 64
     def __init__(self):
         super(Net, self).__init__()
         
@@ -19,13 +68,51 @@ class Net(nn.Module):
         ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
-        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
+       
+        # 1 input image channel (grayscale), 64 output channels/feature maps, 7x7 square convolution kernel
+        self.conv1 = nn.Conv2d(1, 64, 7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(True)
+        self.maxpool = nn.MaxPool2d(3, 2, 1) #(kernel_size,stride,padding)
+        self.layer1 = self._make_layer(BasicBlock, 64, 2) #block, planes, blocks
+        self.layer2 = self._make_layer(BasicBlock, 128, 2, stride=2)
+        self.layer3 = self._make_layer(BasicBlock, 256, 2, stride=2)
+        self.layer4 = self._make_layer(BasicBlock, 512, 2, stride=2)
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+       
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Sequential(
+            Flatten(),
+            nn.Linear(512, 136)
+        )       
+         # initialize       
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)       
         
+        
+        
+        
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion: 
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )            
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
 
+        return nn.Sequential(*layers)        
         
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
@@ -34,4 +121,26 @@ class Net(nn.Module):
         
         
         # a modified x, having gone through all the layers of your model, should be returned
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)       
+        
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = self.fc(x)        
+        
+        
         return x
+    
+if __name__ == '__main__':
+    x = torch.randn(10, 1,10,10) #Input: :math:`(N, C_{in}, H_{in}, W_{in})`
+    net = Net()
+    y = net(x)
+# =============================================================================
+#     from IPython import embed
+#     embed()
+# =============================================================================
